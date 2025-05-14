0a16c232a6d98d52d155785dab48ce50c3e8bfff
Varvinska <sba23247@student.cct.ie>
2025-05-14 22:13:30 +0100

    Update Movie_App.py

--- Movie_App.py
+++ Movie_App.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud
 
 st.set_page_config(page_title="Movie Dashboard", layout="wide")
 
